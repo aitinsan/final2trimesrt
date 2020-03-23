d61d6b6edcd6233d1d69959830232b526c3425d2
aitinsan <57362967+aitinsan@users.noreply.github.com>
2020-03-23 22:24:23 +0600

    Add files via upload

---
+++ Новая папка/endtermInsan/new.js
@@ -0,0 +1,8 @@
+function validate(){
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+if (true){
+alert("Вы написали: "+password);
+window.location = "index.html";
+return false;
+}
